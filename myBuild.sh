d3c09a7ee91fd34a85063d0774b71f68fa95a41b
think3r <think3r@foxmail.com>
2022-12-26 11:18:24 +0800

    add vscode 配置(clangd & 断点调试)

---
+++ myBuild.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+BEAR_MARK=""
+# if [ bear --version | grep ]
+if (uname -a | grep -E "WSL2|Darwin" > /dev/null); then
+   BEAR_MARK="--"
+   echo "this is   WSL2 | Darwin"
+fi
+
+./configure --enable-debug
+
+bear ${BEAR_MARK} make -j8 && bear --append ${BEAR_MARK} make examples -j4
+
+if [[ "$(uname -a)" == *Linux*Microsoft* ]]; then
+   # WSL1 的桌面是挂载的 windows 的桌面 两个路径不一致. /mnt 挂在路径为真正的识别路径, 因此在下方见 /home 路径替换为真正的挂载路径
+   echo -e "\e[1;33mThis WSL. Run compile_commands.json string replace : /home --> /mnt \e[0m"
+   sed -i "s/\/home\/think3r\/Desktop/\/mnt\/e\/Desktop/g" ./compile_commands.json
+fi
